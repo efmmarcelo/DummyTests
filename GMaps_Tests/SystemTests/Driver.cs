71ad01b2ed4a20c87255d2bc359c085339a08c4b
Eduardo Marcelo <eduardo.marcelo@ritain.io>
2022-02-11 17:31:13 +0000

    Adicionar arquivos de projeto.

---
+++ GMaps_Tests/SystemTests/Driver.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SystemTests
+{
+    public class Driver
+    {
+        public static IWebDriver _driver;
+
+        public static IWebDriver OpenDriver(string browserName)
+        {
+            _driver = null;
+
+            switch (browserName)
+            {
+                case "Chrome":
+                    _driver = new ChromeDriver();
+                    _driver.Manage().Window.Maximize();
+                    break;
+                case "Firefox":
+                    _driver = new FirefoxDriver();
+                    _driver.Manage().Window.Maximize();
+                    break;
+                default:
+                    break;
+            }
+
+            if(_driver != null)
+            {
+                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            }
+
+            return _driver;
+        }
+
+        public static IWebElement WaitUntilVisible(By by, IWebDriver driver)
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 10));
+            var searchResultTitle = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(by));
+            return searchResultTitle;
+        }
+    }
+}
---
+++ GMaps_Tests/SystemTests/Pages/HomePage.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SystemTests.Pages
+{
+    class HomePage
+    {
+        private IWebDriver driver;
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void goToGoogleMaps()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com/maps");
+            driver.FindElement(By.XPath("//button[@jsname='higCR']")).Click();
+        }
+
+        public void searchLocation(string location)
+        {
+            IWebElement searchLocationInput = driver.FindElement(By.Id("searchboxinput"));
+            searchLocationInput.SendKeys(location);
+
+            driver.FindElement(By.Id("searchbox-searchbutton")).Click();
+
+            //Sometimes element not yet visble(only present in DOM), so we need to ensure visibility wit wait driver
+            IWebElement searchResultTitle = Driver.WaitUntilVisible(By.XPath($"//h1[contains(@class,'header')][//*[text()='{location}']]"), driver);
+
+            Assert.That(searchResultTitle.Displayed, $"Cannot validate that Left panel has {location} element text visible!");
+            StringAssert.Contains("headline", searchResultTitle.GetAttribute("class"), $"Cannot validate that Left panel has {location} as headline text!");
+        }
+
+        public void validateDirections(string location)
+        {
+            IWebElement directionsBtn = driver.FindElement(By.XPath("//button[contains(@jsaction,'placeActions.directions')]"));
+            Assert.That(directionsBtn.Enabled, "Cannot validate that directions button is clickable!");
+            directionsBtn.Click();
+
+            IWebElement destinationField1 = driver.FindElement(By.XPath($"//*[@id='directions-searchbox-1']//input[contains(@aria-label,'{location}')]"));
+            Assert.That(destinationField1.Displayed, $"Cannot validate that direction '{location}' is on destination field! ");
+        }
+    }
+}
